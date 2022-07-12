950dcfaf904f11f4246c0e8917204b715b9670f6
Evdude33 <62618387+emllewellyn@users.noreply.github.com>
2022-07-12 19:23:46 -0400

    End of Day Push

--- src/main/java/com/sclass/services/BuildService.java
+++ src/main/java/com/sclass/services/BuildService.java
@@ -15,7 +15,6 @@ public class BuildService {
 	
 	public Build createBuild(int userId, String name, int moboId, int cpuId, int ramId, int storageId, int psuId, 
 			int caseId, boolean hasFourRam) throws Exception {
-		// Will need more logic here
 		// Get all parts from DB in build
 		// Check if mobo and cpu have same manufacturer
 		// Check if psu wattage is greater than the sum of the other parts
--- src/main/resources/public/createBuildPageScript.js
+++ src/main/resources/public/createBuildPageScript.js
@@ -1,3 +1,29 @@
+let baseUrl = "http://localhost:8081";
+
+window.onload = function () {
+    console.log("here");
+    // Call populate dropdowns once complete
+    // populateDropdowns();
+}
+
+async function populateDropdowns() {
+    let res = await fetch(
+        `${baseUrl}/parts`,
+        {
+            method: 'GET',
+            header: { 'Content-Type': 'application/json' }
+        }
+    );
+    let resJson = await res.json()
+        .then((resp) => {
+            fillTable(resp);
+        })
+        .catch((error) => {
+            console.log(error);
+            alert("Failed to get parts");
+        });
+}
+
 async function submitBuild(){
-    
+    //TODO
 }
